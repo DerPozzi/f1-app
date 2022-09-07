a4cf00500a29dc1bcc8fda598d164a161c87258c
Gianluca Pozzo <gpozzo@web.de>
2022-09-07 19:52:58 +0200

    ✨

--- src/index.js
+++ src/index.js
@@ -2,38 +2,23 @@ const root = document.querySelector('#root');
 const link = document.getSelection('a');
 
 window.onload = async () => {
-	let hash = window.location.hash;
+	const { hash } = window.location;
 
 	if (hash === '#season') {
-		let season_result = await season();
-		root.innerHTML =
-			'<ul>' +
-			season_result
-				.map((race) => {
-					return '<li class="race">' + race.raceName + '</li>';
-				})
-				.join(' ') +
-			'</ul>';
+		const season_result = await season();
+		root.innerHTML = `<ul>${season_result
+			.map((race) => `<li class="race">${race.raceName}</li>`)
+			.join(' ')}</ul>`;
 	} else if (hash === '#drivers') {
-		let drivers_result = await drivers();
-		root.innerHTML =
-			'<ul>' +
-			drivers_result
-				.map((driver) => {
-					return '<li class="driver">' + driver.code + '</li>';
-				})
-				.join(' ') +
-			'</ul>';
+		const drivers_result = await drivers();
+		root.innerHTML = `<ul>${drivers_result
+			.map((driver) => `<li class="driver">${driver.code}</li>`)
+			.join(' ')}</ul>`;
 	} else if (hash === '#constructors') {
-		let constructors_result = await constructors();
-		root.innerHTML =
-			'<ul>' +
-			constructors_result
-				.map((constructor) => {
-					return '<li class="constructor">' + constructor.name + '</li>';
-				})
-				.join(' ') +
-			'</ul>';
+		const constructors_result = await constructors();
+		root.innerHTML = `<ul>${constructors_result
+			.map((constructor) => `<li class="constructor">${constructor.name}</li>`)
+			.join(' ')}</ul>`;
 	} else {
 		root.innerHTML =
 			'<h1>Herzlich Willkommen!</h1> <h2>Danke, dass du die App benutzt</h2>';
@@ -41,52 +26,32 @@ window.onload = async () => {
 };
 
 window.onhashchange = async () => {
-	let hash = window.location.hash;
+	const { hash } = window.location;
 
 	switch (hash) {
 		case '#season':
-			let season_result = await season();
-			root.innerHTML =
-				'<ul>' +
-				season_result
-					.map((race) => {
-						return '<li class="race">' + race.raceName + '</li>';
-					})
-					.join(' ') +
-				'</ul>';
+			const season_result = await season();
+			root.innerHTML = `<ul>${season_result
+				.map((race) => `<li class="race">${race.raceName}</li>`)
+				.join(' ')}</ul>`;
 			break;
 		case '#drivers':
-			let drivers_result = await drivers();
-			root.innerHTML =
-				'<ul>' +
-				drivers_result
-					.map((driver) => {
-						return (
-							'<li class="driver">' +
-							' ' +
-							driver.permanentNumber +
-							' ' +
-							driver.code +
-							' ' +
-							driver.givenName +
-							' ' +
-							driver.familyName +
-							'</li>'
-						);
-					})
-					.join(' ') +
-				'</ul>';
+			const drivers_result = await drivers();
+			root.innerHTML = `<ul>${drivers_result
+				.map(
+					(driver) =>
+						'<li class="driver">' +
+						` ${driver.permanentNumber} ${driver.code} ${driver.givenName} ${driver.familyName}</li>`
+				)
+				.join(' ')}</ul>`;
 			break;
 		case '#constructors':
-			let constructors_result = await constructors();
-			root.innerHTML =
-				'<ul>' +
-				constructors_result
-					.map((constructor) => {
-						return '<li class="constructor">' + constructor.name + '</li>';
-					})
-					.join(' ') +
-				'</ul>';
+			const constructors_result = await constructors();
+			root.innerHTML = `<ul>${constructors_result
+				.map(
+					(constructor) => `<li class="constructor">${constructor.name}</li>`
+				)
+				.join(' ')}</ul>`;
 			break;
 		default:
 			root.innerHTML = 'default';
